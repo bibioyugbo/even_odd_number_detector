01532ff75009b9e2cde0418f40ce519d2db4795a
bibioyugbo <obehioyeoyugbo@gmail.com>
2021-07-12 13:54:27 +0100

    implemented evenNumberDetection function

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+//entry point
+fun main()
+{
+    oddnumberdetector()
+    evenNumberDetection()
+}
+
+fun evenNumberDetection()
+{
+    val number = 6
+
+    if (number % 2 == 0)
+
+    {
+        print("It is an even number")
+    }
+}
+
+fun oddnumberdetector()
+{
+    val number = 11
+    if (number % 2 !== 0)
+    {
+        println("This is an odd number")
+    }
+}
